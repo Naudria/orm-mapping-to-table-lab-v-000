bccf20b0a3207272bfa44a410ca7a837ab51de9a
Rima Jean <rima.jean@gmail.com>
2018-11-06 19:38:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,4 +9,31 @@ class Student
     @grade = grade
     @id = id
   end
+
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL 
+    DB[:conn].execute(sql)
+    end 
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+    end
+
+  def save
+    sql = <<-SQL 
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL 
+    DB[:conn].execute(sql, @name, @grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
+  end
+
+  def self.create(name:, grade:)
+  end
 end
